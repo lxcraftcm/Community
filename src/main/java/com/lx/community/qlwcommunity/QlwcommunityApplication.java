98c0e8402571b9500f8333d3402313ea67ee598e
lxcraftcm <lxcraftcm@163.com>
2019-11-07 20:37:29 +0800

    first commit

---
+++ src/main/java/com/lx/community/qlwcommunity/QlwcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.lx.community.qlwcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlwcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QlwcommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lx/community/qlwcommunity/QlwcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lx.community.qlwcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlwcommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
